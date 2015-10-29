e9fcfa8de0f341fb537a857b6f364bd42314badc
MaskedRpg <masked.rpg@gmail.com>
2015-10-29 11:52:33 -0200

    Update MBS_VisualEquipment.js

--- mv/MBS_VisualEquipment.js
+++ mv/MBS_VisualEquipment.js
@@ -8,129 +8,134 @@
 // Plugin specifications (Do not modify!)
 //
 /*:
-
-  @author Masked
-  @plugindesc Shows images on characters depending on their equipments.
-  @help
-  =============================================================================
-  Introduction
-  =============================================================================
-  This script creates specific images on characters that makes them look like
-  if it really had that items equipped.
-
-  =============================================================================
-  How to use
-  =============================================================================
-  You can choose the charset that will represent an item by putting this tag on
-  its notes:
-
-  <graphic:equip_graphic.png, x>
-
-  The "equip_graphic.png" part is the file name, the file have to be placed on 
-  the folder you choose on the plugin settings. The "x" is the character index,
-  if it's a single char (the file name starts with '$'), set this to 0.
-  The equipment charset works the same way as every other charset on the game,
-  so you can use $ and ! to specify big characters and stuff.
-
-  You can set an offset for the equipment graphic if you want by putting this
-  tag on the equip's note:
-
-  <offset:x, y>
-
-  The "x" and "y" are the horizontal and vertical offset for the equipment
-  image.
-
-  And last but not least, you can set the equipment graphic's priority for
-  showing some equipments above others or even below the character, just
-  put this tag on its notes:
-
-  <priority:z>
-
-  The "z" is the equipment priority, higher priorities make equipments to be
-  draw above the others and negative priorities will make it to be drawn 
-  below the character.
-
-  If you want, you can also use the following tag on an event's notes to make
-  the script draw equipments on it:
-
-  <equips:a id1, w id2, a id3...>
-
-  The equipments are specified by using the first letter of the equipment 
-  type (that is, a for Armor and w for Weapon) followed by its ID. Ex.:
-  <equips: a 1, w 2>
-
-  In this example, the event would equip the armor with ID 1 and the weapon 
-  with ID 2 from the database.
-
-  =============================================================================
-  Credits
-  =============================================================================
-  - Masked, creator.
-
-  @param Equipments Path
-  @desc Folder where the equipment charsets will be located.
-  @default ./img/equips/
-
-*/
+ *
+ * @author Masked
+ * @plugindesc Shows images on characters depending on their equipments.         
+ *
+ * <MBS VisualEquip>
+ * @help
+ * =============================================================================
+ * Introduction
+ * =============================================================================
+ * This script creates specific images on characters that makes them look like
+ * if it really had that items equipped.
+ *
+ * =============================================================================
+ * How to use
+ * =============================================================================
+ * You can choose the charset that will represent an item by putting this tag on
+ * its notes:
+ *
+ * <graphic:equip_graphic.png, x>
+ *
+ * The "equip_graphic.png" part is the file name, the file have to be placed on 
+ * the folder you choose on the plugin settings. The "x" is the character index,
+ * if it's a single char (the file name starts with '$'), set this to 0.
+ * The equipment charset works the same way as every other charset on the game,
+ * so you can use $ and ! to specify big characters and stuff.
+ *
+ * You can set an offset for the equipment graphic if you want by putting this
+ * tag on the equip's note:
+ *
+ * <offset:x, y>
+ *
+ * The "x" and "y" are the horizontal and vertical offset for the equipment
+ * image.
+ *
+ * And last but not least, you can set the equipment graphic's priority for
+ * showing some equipments above others or even below the character, just
+ * put this tag on its notes:
+ *
+ * <priority:z>
+ *
+ * The "z" is the equipment priority, higher priorities make equipments to be
+ * draw above the others and negative priorities will make it to be drawn 
+ * below the character.
+ *
+ * If you want, you can also use the following tag on an event's notes to make
+ * the script draw equipments on it:
+ *
+ * <equips:a id1, w id2, a id3...>
+ *
+ * The equipments are specified by using the first letter of the equipment 
+ * type (that is, a for Armor and w for Weapon) followed by its ID. Ex.:
+ * <equips: a 1, w 2>
+ *
+ * In this example, the event would equip the armor with ID 1 and the weapon 
+ * with ID 2 from the database.
+ *
+ * =============================================================================
+ * Credits
+ * =============================================================================
+ * - Masked, creator.
+ *
+ * @param Equipments Path
+ * @desc Folder where the equipment charsets will be located.
+ * @default ./img/equips/
+ *
+ */
 /*:pt
-
-  @author Masked
-  @plugindesc Cria imagens sobre o personagem dependendo dos equipamentos dele.
-  @help
-  =============================================================================
-  Introdução
-  =============================================================================
-  Este script cria imagens especiais sobre o personagem que variam de acordo
-  com os equipamentos que ele está usando no momento.
-
-  =============================================================================
-  Utilização
-  =============================================================================
-  Para definir o nome arquivo de imagem da qual o gráfico do equipamento vem
-  adicione nas notas do equipamento:
-
-  <graphic:equip_graphic.png, x>
-
-  Sendo o 'equip_graphic.png' o nome do arquivo de gráfico na pasta configurada
-  no plugin e o 'x' o índice do char dentro do set.
-  Os gráficos de equipamento funcionam da mesma forma que os de personagem e os
-  prefixos de gráfico ($ e !) também funcionam com ele.
-
-  Você pode ainda definir um offset para o equipamento com a tag offset:
-
-  <offset:x, y>
-
-  Sendo o x e y as cordenadas para o offset. Também pode usar a tag priority
-  para colocar equipamentos mais acima ou abaixo na tela:
-
-  <priority:z>
-
-  Quando maior o z maior a prioridade do equipamento e ele aparece mais acima
-  do resto. Índices negativos fazem com que o gráfico apareça abaixo do
-  personagem.
-
-  Caso queira adicionar um gráfico de equipamento a um evento, adicione nas
-  notas dele a tag:
-
-  <equips:a id1, w id2, a id3...>
-
-  Os ids devem vir precedidos pela inicial do tipo de equipamento seguido por
-  um espaço, para armaduras, use 'a id', e para armas use 'w id'. Ex.:
-  <equips: a 1, w 2>
-
-  Neste exemplo o evento usaria a armadura de ID 1 e arma de ID 2.
-  Você pode por quantos IDs quiser, basta separá-los por vírgulas.
-
-  =============================================================================
-  Créditos
-  =============================================================================
-  - Masked, por criar
-
-  @param Equipments Path
-  @desc Caminho da pasta dos arquivos de gráfico de equipamento
-  @default ./img/equips/
-
-*/
+ *
+ * @author Masked
+ * @plugindesc Cria imagens sobre o personagem dependendo dos equipamentos dele. 
+ *
+ * <MBS VisualEquip>
+ *
+ * @help
+ * =============================================================================
+ * Introdução
+ * =============================================================================
+ * Este script cria imagens especiais sobre o personagem que variam de acordo
+ * com os equipamentos que ele está usando no momento.
+ *
+ * =============================================================================
+ * Utilização
+ * =============================================================================
+ * Para definir o nome arquivo de imagem da qual o gráfico do equipamento vem
+ * adicione nas notas do equipamento:
+ *
+ * <graphic:equip_graphic.png, x>
+ *
+ * Sendo o 'equip_graphic.png' o nome do arquivo de gráfico na pasta configurada
+ * no plugin e o 'x' o índice do char dentro do set.
+ * Os gráficos de equipamento funcionam da mesma forma que os de personagem e os
+ * prefixos de gráfico ($ e !) também funcionam com ele.
+ *
+ * Você pode ainda definir um offset para o equipamento com a tag offset:
+ *
+ * <offset:x, y>
+ *
+ * Sendo o x e y as cordenadas para o offset. Também pode usar a tag priority
+ * para colocar equipamentos mais acima ou abaixo na tela:
+ *
+ * <priority:z>
+ *
+ * Quando maior o z maior a prioridade do equipamento e ele aparece mais acima
+ * do resto. Índices negativos fazem com que o gráfico apareça abaixo do
+ * personagem.
+ *
+ * Caso queira adicionar um gráfico de equipamento a um evento, adicione nas
+ * notas dele a tag:
+ *
+ * <equips:a id1, w id2, a id3...>
+ *
+ * Os ids devem vir precedidos pela inicial do tipo de equipamento seguido por
+ * um espaço, para armaduras, use 'a id', e para armas use 'w id'. Ex.:
+ * <equips: a 1, w 2>
+ *
+ * Neste exemplo o evento usaria a armadura de ID 1 e arma de ID 2.
+ * Você pode por quantos IDs quiser, basta separá-los por vírgulas.
+ *
+ * =============================================================================
+ * Créditos
+ * =============================================================================
+ * - Masked, por criar
+ *
+ * @param Equipments Path
+ * @desc Caminho da pasta dos arquivos de gráfico de equipamento
+ * @default ./img/equips/
+ *
+ */
 
 var Imported = Imported || {};
 
@@ -141,7 +146,7 @@ MBS.VisualEquipment = {};
 
 (function ($) {
 
-  $.Parameters = PluginManager.parameters("MBS_VisualEquipment");
+  $.Parameters = $plugins.filter(function(p) {return p.description.contains('<MBS VisualEquip>');})[0].parameters;
   $.Param = $.Param || {};
 
   //-----------------------------------------------------------------------------
@@ -163,7 +168,7 @@ MBS.VisualEquipment = {};
    * @return Retorna um bitmap de uma imagem de equipamento
    */
   ImageManager.loadEquipment = function (filename, hue) {
-    return this.loadBitmap($.Param.path, filename, hue, true);
+    return this.loadBitmap($.Param.path, filename, hue, false);
   };
 
   //-----------------------------------------------------------------------------
